f14f4c98fb7bf0f5aca0df5192a7d7d364ac944a
ДеЮра <doomov@yandex.ru>
2025-02-05 17:18:20 +0700

    Update Program.cs

--- ConsoleSecondDigitOfTheNumber 001/Program.cs
+++ ConsoleSecondDigitOfTheNumber 001/Program.cs
@@ -4,52 +4,52 @@
     {
         static void Main(string[] args)
         {
-			// Считываем число n
-			int n;
+		// Считываем число n
+		int n;
 
-			while (true) // Начинаем бесконечный цикл
+		while (true) // Начинаем бесконечный цикл
+		{
+			//Console.WriteLine("Введите число:");
+			if (int.TryParse(GetInput(), out n))
 			{
-				//Console.WriteLine("Введите число:");
-				if (int.TryParse(GetInput(), out n))
-				{
-					n = CalculateSecondLastDigit(n);
-					Console.ReadKey();
-					// Завершаем программу после корректного ввода
-					return; // Выходим из метода Main, завершив программу
-				}
-				else
-				{
+				n = CalculateSecondLastDigit(n);
+				Console.ReadKey();
+				// Завершаем программу после корректного ввода
+				return; // Выходим из метода Main, завершив программу
+			}
+			else
+			{
 					Console.WriteLine("Ошибка: введено некорректное число. Пожалуйста, попробуйте снова.");
-				}
 			}
-		}
-
-		/// <summary>
-		/// Вычисляет вторую цифру числа.
-		/// </summary>
-		/// <param name="number">Число, из которого нужно получить вторую цифру.</param>
-		/// <returns>Вторая цифра числа.</returns>
-		private static int CalculateSecondLastDigit(int n)
-		{
-			// Уменьшаем n, пока оно больше 99
-			while (n > 99)
-			{
-				n /= 10; // Делим на 10
-			}
-
-			// Выводим последнюю цифру n
-			Console.WriteLine("Последняя цифра: " + (n % 10));
-			return n;
-		}
-
-		/// <summary>
-		/// Запрашивает у пользователя ввод трёхзначного числа.
-		/// </summary>
-		/// <returns>Строку, введённую пользователем.</returns>
-		static string? GetInput()
-		{
-			Console.WriteLine("Введите трёхзначное число:");
-			return Console.ReadLine();
 		}
 	}
+
+	/// <summary>
+	/// Вычисляет вторую цифру числа.
+	/// </summary>
+	/// <param name="number">Число, из которого нужно получить вторую цифру.</param>
+	/// <returns>Вторая цифра числа.</returns>
+	private static int CalculateSecondLastDigit(int n)
+	{
+		// Уменьшаем n, пока оно больше 99
+		while (n > 99)
+		{
+			n /= 10; // Делим на 10
+		}
+
+		// Выводим последнюю цифру n
+		Console.WriteLine("Последняя цифра: " + (n % 10));
+		return n;
+	}
+
+	/// <summary>
+	/// Запрашивает у пользователя ввод трёхзначного числа.
+	/// </summary>
+	/// <returns>Строку, введённую пользователем.</returns>
+	static string? GetInput()
+	{
+			Console.WriteLine("Введите трёхзначное число:");
+			return Console.ReadLine();
+	}
+   }
 }
